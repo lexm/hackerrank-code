c41366d8573e473c59f131a5944a7691e5f54b29
Lex Myers <lex.myers@gmail.com>
2017-03-24 17:49:39 -0700

    Insertion Sort 1: printArray function works; start of sort

--- Algorithms/Sorting/insertionsort1.js
+++ Algorithms/Sorting/insertionsort1.js
@@ -1,7 +1,26 @@
+function printArray(arr) {
+    var len = arr.length;
+    var out = '';
+    for(i = 0; i < len; i++) {
+        out += arr[i];
+        if(i < len - 1) {
+            out += ' ';
+        }
+    }
+    console.log(out);
+}
+
 function processData(input) {
     input = input.split('\n');
     var size = parseInt(input[0]);
     var arr = input[1].split(' ').map(function(cur) {return parseInt(cur);});
+    var idx = arr.length - 1
+    var newNum = arr[idx];
+    while(arr[idx - 1] > newNum) {
+        arr[idx] = arr[idx - 1];
+        idx = idx - 1;
+        printArray(arr);
+    }
     console.log('size:', size);
     console.log('arr: ', arr);
 }
