451cb39e2ef168cdcbbc9a56b775eb6449eac732
Lex Myers <lex.myers@gmail.com>
2017-04-03 09:59:47 -0700

    first stab at anagrams problem

---
+++ CTCI/making-anagrams.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var alphString = 'abcdefghijklmnopqrstuvwxyz'
+    var resultArray = [];
+    for(i = 0; i < alphString.length; i++) {
+        resultArray.push(0);
+    }
+    var a = readLine();
+    var b = readLine();
+    a.split('').forEach(function(char) {
+        resultArray[alphString.indexOf(char)]++;
+    });
+    b.split('').forEach(function(char) {
+        resultArray[alphString.indexOf(char)]--;
+    });
+    var result = resultArray.reduce(function(acc, cur) {
+        return acc + Math.abs(cur);
+    });
+    console.log(result);
+}
