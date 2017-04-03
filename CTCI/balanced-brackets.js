2245959c288e4fa88718232d61c80beca46d9f18
Lex Myers <lex.myers@gmail.com>
2017-04-03 11:18:40 -0700

    first try at Balanced Brackets

---
+++ CTCI/balanced-brackets.js
@@ -0,0 +1,58 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+function isBalanced(expression) {
+    expression = expression.split('');
+    var stack = [];
+    for(i = 0; i < expression.length; i++) {
+        var lastItem = stack.slice(-1)[0];
+        switch(expression[i]) {
+            case '{':
+            case '(':
+            case '[':
+                stack.push(expression[i]);
+                break;
+            case '}':
+            case ')':
+            case ']':
+                if(expression[i] === lastItem) {
+                    stack.pop();
+                    break;
+                }
+            default:
+                return false;
+        }
+        return true;
+    }
+}
+
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var expression = readLine();
+        if(isBalanced(expression)) {
+            console.log('YES');
+        } else {
+            console.log('NO');
+        }
+    }
+}
