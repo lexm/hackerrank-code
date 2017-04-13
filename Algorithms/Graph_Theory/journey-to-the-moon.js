5bbb23ff44f3057d30fe45de0497caf6f0a7116d
Lex Myers <lex.myers@gmail.com>
2017-04-13 10:56:19 -0700

    now generates nodeArray

--- Algorithms/Graph_Theory/journey-to-the-moon.js
+++ Algorithms/Graph_Theory/journey-to-the-moon.js
@@ -35,11 +35,12 @@ function processData(input) {
     var inputArray = new InputArray(input);
     var n, p, a, b;
     [n, p] = inputArray.readLine().split(' ').map(Number);
-    console.log(n, p);
+    var nodeArray = new NodeArray(n);
     for(a0 = 0; a0 < p; a0++) {
         [a, b] = inputArray.readLine().split(' ').map(Number);
-        console.log(a, b);
+        nodeArray.addEdge(a, b);
     }
+    console.log(nodeArray);
 }
 
 process.stdin.resume();
