ce9d5f68c27ab16e010b73e94dc23b7e943234d3
Lex Myers <lex.myers@gmail.com>
2017-06-19 21:58:02 -0700

    Solution to Python > Strings > sWAP cASE (take 2)

--- Python/Strings/swap-case.py
+++ Python/Strings/swap-case.py
@@ -1,8 +1,2 @@
-def swap_char(ch):
-    if ch.isupper():
-        return ch.lower()
-    else:
-        return ch.upper()
-
 def swap_case(s):
-    return ''.join(list(map(swap_char, list(s))))
+    return s.swapcase()
