450f3839ad8adb93d1ef8ecc0d4fef8dac067615
Lex Myers <lex.myers@gmail.com>
2017-06-24 00:58:02 -0700

    refactored Merge the Tools

--- Python/Strings/merge-the-tools.py
+++ Python/Strings/merge-the-tools.py
@@ -1,16 +1,8 @@
 def merge_the_tools(string, k):
-    length = len(string)
-    segs = length // k
     seg = ''
-    arr = []
-    for i in range(0, length):
-        seg += string[i]
+    for i in range(0, len(string)):
+        if(not(string[i] in seg)):
+            seg += string[i]
         if(((i + 1)% k == 0) and (i > 0 or k == 1)):
-            arr.append(seg)
+            print(seg)
             seg = ''
-    for seg in arr:
-        res = ''
-        for ch in seg:
-            if(not(ch in res)):
-                res += ch
-        print(res)
