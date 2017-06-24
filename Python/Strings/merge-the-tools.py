e2e65df5e19cb16142121ae7849bbf417125ec2d
Lex Myers <lex.myers@gmail.com>
2017-06-24 00:51:38 -0700

    Solution to Python > Strings > Merge the Tools! (take 2)

--- Python/Strings/merge-the-tools.py
+++ Python/Strings/merge-the-tools.py
@@ -1,11 +1,11 @@
 def merge_the_tools(string, k):
     length = len(string)
-    seg_length = length // k
+    segs = length // k
     seg = ''
     arr = []
     for i in range(0, length):
         seg += string[i]
-        if(((i + 1)% seg_length == 0) and i > 0):
+        if(((i + 1)% k == 0) and i > 0):
             arr.append(seg)
             seg = ''
     for seg in arr:
