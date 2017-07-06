7a8689f84bfc7787a96b9fe66861e7e5d93bc425
Lex Myers <lex.myers@gmail.com>
2017-07-05 19:37:38 -0700

    Solution to Python > Sets > Symmetric Difference

---
+++ Python/Sets/symmetric-difference.py
@@ -0,0 +1,10 @@
+m = int(input())
+set1 = set(map(int, input().split(' ')))
+n = int(input())
+set2 = set(map(int, input().split(' ')))
+diff1 = set1.difference(set2)
+diff2 = set2.difference(set1)
+diff = diff1.union(diff2)
+diff_list = sorted(list(diff))
+for x in diff_list:
+    print(x)
