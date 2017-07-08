894694af8ac89c11011a3e9fd848b424af42331e
Lex Myers <lex.myers@gmail.com>
2017-07-08 10:45:48 -0700

    add code to make regex problem run live

--- Regex/Character_Class/matching-specific-characters.py
+++ Regex/Character_Class/matching-specific-characters.py
@@ -1,3 +1,5 @@
-Regex_Pattern = r'[123][012][0sx][03aA][sux][\.\,]' # Do not delete 'r'.
+Regex_Pattern = r'[123][012][0sx][03aA][sux][.,]' # Do not delete 'r'.
 
+import re
 
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
