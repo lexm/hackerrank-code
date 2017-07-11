58e741e72e3e5237a3dae545054a1c7a6995d3b6
Lex Myers <lex.myers@gmail.com>
2017-07-11 15:56:22 -0700

    Second attempt at Python > Collections > Most Common Score: 18.00

--- Python/Collections/most-commons.py
+++ Python/Collections/most-commons.py
@@ -1,5 +1,8 @@
 from collections import Counter
+from operator import itemgetter
 S = input()
-mc = Counter(S).most_common(3)
-for pair in mc:
+mc = list(Counter(S).most_common(3))
+smc1 = sorted(mc, key=itemgetter(0))
+smc2 = sorted(smc1, key=itemgetter(1), reverse=True)
+for pair in smc2:
     print(' '.join(map(str, pair)))
