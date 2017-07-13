272951731c912edc81f329f4a9b4a882fa9e2277
Lex Myers <lex.myers@gmail.com>
2017-07-12 23:19:38 -0700

    Refactored solution to Python > Built-Ins > Any or All

--- Python/Built-Ins/any-or-all.py
+++ Python/Built-Ins/any-or-all.py
@@ -1,9 +1,3 @@
-def pal(number):
-    numstr = str(number)
-    if numstr == numstr[::-1]:
-        return True
-    return False
-
 N = int(input())
 nums = list(map(int, input().split()))
-print(all(list(map(lambda x: x >= 0, nums))) and any(list(map(pal, nums))))
+print(all(list(map(lambda x: x >= 0, nums))) and any(list(map(lambda x: str(x) == str(x)[::-1], nums))))
