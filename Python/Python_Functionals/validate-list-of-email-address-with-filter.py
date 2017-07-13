8e4ccff6c50f9d6c7563723ed990f402022f9b1c
Lex Myers <lex.myers@gmail.com>
2017-07-13 14:45:01 -0700

    Refactored Solution to Python > Python Functionals > Validating Email Addresses With a Filter

--- Python/Python_Functionals/validate-list-of-email-address-with-filter.py
+++ Python/Python_Functionals/validate-list-of-email-address-with-filter.py
@@ -1,18 +1,12 @@
 def fun(s):
     arr1 = s.split('@')
-    if(len(arr1) != 2):
+    if(len(arr1) != 2 or len(arr1[0]) == 0):
         return False
-    if(len(arr1[0]) == 0):
-        return False
-    user_arr = list(arr1[0])
-    fil_user = list(filter(lambda ch: (ch.isalnum() or ch == '_' or ch == '-') == False, user_arr))
-    if (len(fil_user) > 0):
+    if(len(list(filter(lambda ch: (ch.isalnum() or ch == '_' or ch == '-') == False, list(arr1[0])))) > 0):
         return False
     arr2 = arr1[1].split('.')
     if(len(arr2) != 2):
         return False
-    if(arr2[0].isalnum() == False):
-        return False
-    if(len(arr2[1]) > 3):
+    if(arr2[0].isalnum() == False or len(arr2[1]) > 3):
         return False
     return True
