2f2df6be2fdde32894768172d0c224a3da861e02
Lex Myers <lex.myers@gmail.com>
2017-07-15 15:19:30 -0700

    Solution to Python > Regex and Parsing > Re.findall() & Re.finditer()

--- Python/Regex_and_Parsing/re-findall-re-finditer.py
+++ Python/Regex_and_Parsing/re-findall-re-finditer.py
@@ -2,5 +2,8 @@ import re
 S = input()
 cons = "[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm]"
 arr = re.findall(r"(?<=" + cons + ")([aeiouAEIOU]{2,})(?=" + cons + ")", S)
-for hit in arr:
-    print(hit)
+if(arr):
+    for hit in arr:
+        print(hit)
+else:
+    print(-1)
