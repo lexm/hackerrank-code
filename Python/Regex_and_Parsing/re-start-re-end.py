187f296c68ea8dc1c650333ec5d405739640d72f
Lex Myers <lex.myers@gmail.com>
2017-07-17 17:31:24 -0700

    Attempt 2: Python > Regex and Parsing > Re.start() & Re.end()

--- Python/Regex_and_Parsing/re-start-re-end.py
+++ Python/Regex_and_Parsing/re-start-re-end.py
@@ -1,9 +1,10 @@
 import re
 S = input()
 k = input()
-#klook = '(?=(' + k + '))'
 pat = re.compile('(?=(' + k + '))')
 ms = pat.finditer(S)
-#ms = re.search(S, k)
-for m in ms:
-    print((m.start(), m.start() + len(k) -1))
+if(ms):
+    for m in ms:
+        print((m.start(), m.start() + len(k) -1))
+else:
+    print((-1, -1))
