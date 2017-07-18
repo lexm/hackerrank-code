82ff1f0202ae7465aaae8bdaccbfbb74b49676b1
Lex Myers <lex.myers@gmail.com>
2017-07-18 16:29:23 -0700

    Attempt 1: Python > Regex and Parsing > Validating and Parsing Email Addresses

---
+++ Python/Regex_and_Parsing/validating-named-email-addresses.py
@@ -0,0 +1,9 @@
+import re
+import email.utils
+n = int(input())
+pattern = r"[a-zA-Z][\w\-.]*@[a-zA-Z]+\.[a-zA-Z]{1,3}"
+for _ in range(0,n):
+    line = input()
+    parsed = email.utils.parseaddr(line)
+    if(re.match(pattern, parsed[1])):
+        print(line)
