d8ab81dccbcbac7c1a8739488acf6f889438a8fd
Lex Myers <lex.myers@gmail.com>
2017-07-17 20:02:35 -0700

    Solution to Python > Regex and Parsing > Regex Substitution

--- Python/Regex_and_Parsing/re-sub-regex-substitution.py
+++ Python/Regex_and_Parsing/re-sub-regex-substitution.py
@@ -2,12 +2,12 @@ import re
 
 def makeandor(match):
     txt = str(match.group(0))
-    if txt == ' && ':
-        return ' and '
-    elif txt == ' || ':
-        return ' or '
+    if txt == '&&':
+        return 'and'
+    elif txt == '||':
+        return 'or'
     
 N = int(input())
 for _ in range(0,N):
     line = input()
-    print(re.sub(r"(\ \&\&\ |\ \|\|\ )", makeandor, line))
+    print(re.sub(r"(?<=\ )(\&\&|\|\|)(?=\ )", makeandor, line))
