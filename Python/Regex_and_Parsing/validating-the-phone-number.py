417339dfedf4932e590ac5147cb19062036e65f5
Lex Myers <lex.myers@gmail.com>
2017-07-18 15:21:56 -0700

    Attempt 1: Python > Regex and Parsing > Validating phone numbers

---
+++ Python/Regex_and_Parsing/validating-the-phone-number.py
@@ -0,0 +1,9 @@
+import re
+N = int(input())
+pattern = r"[7-9]\d{9}"
+for _ in range(0,N):
+    line = input()
+    if(re.match(pattern, line)):
+        print('YES')
+    else:
+        print('NO')
