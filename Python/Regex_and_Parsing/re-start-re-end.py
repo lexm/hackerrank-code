1096f066260ba426b32572ba5cb85fbf3e05a264
Lex Myers <lex.myers@gmail.com>
2017-07-17 17:32:33 -0700

    Solution to Python > Regex and Parsing > Re.start() & Re.end()

--- Python/Regex_and_Parsing/re-start-re-end.py
+++ Python/Regex_and_Parsing/re-start-re-end.py
@@ -3,8 +3,10 @@ S = input()
 k = input()
 pat = re.compile('(?=(' + k + '))')
 ms = pat.finditer(S)
-if(ms):
-    for m in ms:
-        print((m.start(), m.start() + len(k) -1))
-else:
+count = 0
+for m in ms:
+    count += 1
+    print((m.start(), m.start() + len(k) -1))
+if count == 0:
     print((-1, -1))
+
