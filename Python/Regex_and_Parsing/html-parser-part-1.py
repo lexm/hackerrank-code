e076b9e436a3d126c16f8e408a34406698841125
Lex Myers <lex.myers@gmail.com>
2017-07-19 19:56:26 -0700

    Attempt 2: Python > Regex and Parsing > HTML Parser - Part 1

--- Python/Regex_and_Parsing/html-parser-part-1.py
+++ Python/Regex_and_Parsing/html-parser-part-1.py
@@ -1,4 +1,5 @@
 from html.parser import HTMLParser
+import re
 
 class MyParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -10,9 +11,29 @@ class MyParser(HTMLParser):
     def handle_startendtag(self, tag, attrs):
         print("Empty :", tag)
         
+compat1 = r"(.*)(<!--)(.*)"
+compat2 = r"(.*)(-->)(.*)"
+        
 parser = MyParser()
 
 N = int(input())
+comm = False
 for _ in range(0,N):
     line = input()
-    parser.feed(line)
+    if not comm:
+        srch1 = re.search(compat1, line)
+        if srch1:
+            print(srch1.group(1), srch1.group(2), srch1.group(3))
+            parser.feed(srch1.group(1))
+            comm = True
+            line = srch1.group(3)
+        else:
+            parser.feed(line)
+    if comm:
+        srch2 = re.search(compat2, line)
+        if srch2:
+            print(srch2.group(1), srch2.group(2), srch2.group(3))
+            parser.feed(srch2.group(3))
+            comm = False
+
+        
