67849663225de129b025c86fdb5e9314fbf008a1
Lex Myers <lex.myers@gmail.com>
2017-07-24 18:28:44 -0700

    Attempt 2: Python > XML > XML2 - Find the Maximum Depth

--- Python/XML/xml2-find-the-maximum-depth.py
+++ Python/XML/xml2-find-the-maximum-depth.py
@@ -1,8 +1,7 @@
 maxdepth = 0
 def depth(elem, level):
     global maxdepth
-    if elem is not None:
-        level += 1
+    level += 1
     if(maxdepth < level):
         maxdepth = level
     for i in range(0, len(elem)):
